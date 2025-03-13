eb08bcf1474cf695ba9920fdcb8f39048610cd22
Joni Hiltunen <djonih@gmail.com>
2025-03-13 15:52:16 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
+    nixosConfigurations.enne = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      # specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        ./nvidia.nix
+	home-manager.nixosModules.home-manager {
+	   home-manager.useGlobalPkgs = true;
+           home-manager.useUserPackages = true;
+
+           home-manager.users.sose = import ./home.nix;
+      	}
+      ];
+    };
+  };
+}
---
+++ nvidia.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+{
+  hardware.graphics = {
+    enable = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = false;
+
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+  };
+
+}
